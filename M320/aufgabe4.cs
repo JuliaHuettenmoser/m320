7d54d39dc470826106badf6bc34b28573c00de84
JuliaHuettenmoser <112378948+JuliaHuettenmoser@users.noreply.github.com>
2022-10-31 01:09:38 +0100

    Update Aufgabe 4 incomplete

--- M320/aufgabe4.cs
+++ M320/aufgabe4.cs
@@ -6,17 +6,105 @@ namespace test
     {
         static void calculate(string[] args)
 
-        {
-            useParam(1, 2, 3, 4);
-        }
+                    int zahla;
+            int zahlb;
+            int ergebnis;
+            int anzahl;
+            int i = 0;
+            String eingabe;
 
-        public static int useParam (params int[] list)
-        {
-            for(int i  = 0; i < list.Length; i++)
+            Console.Write("Geben Sie an wieviele Nummer Sie eigeben wollen: ");
+            anzahl = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Geben sie den Operator ein: ");
+            eingabe = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Geben Sie die 1. Zahl ein: ");
+            zahla = Convert.ToInt32(Console.ReadLine());
+
+
+            if (eingabe == "+")
             {
-                list[i] += i;
-                Console.WriteLine(list[i]);
+                Console.WriteLine("Geben Sie die nächste Zahl ein: ", i);
+                zahlb = Convert.ToInt32(Console.ReadLine());
+                i++;
+                while (i >= anzahl)
+                {
+                    i++;
+
+                    Console.WriteLine("Geben Sie die nächste Zahl ein: ", i);
+                    zahlb = Convert.ToInt32(Console.ReadLine());
+
+                    zahlb += zahlb;
+
+                }
+
+                ergebnis = zahla + zahlb;
+
+                Console.WriteLine("Das Ergbenis ist: " + zahla + zahlb);
             }
-        }
+
+            else if (eingabe == "-")
+            {
+                Console.WriteLine("Geben Sie die nächste Zahl ein: ");
+                zahlb = Convert.ToInt32(Console.ReadLine());
+                i++;
+                while (i >= anzahl)
+                {
+                    i++;
+
+                    Console.WriteLine("Geben Sie die nächste Zahl ein: ");
+                    zahlb = Convert.ToInt32(Console.ReadLine());
+
+                    zahlb -= zahlb;
+
+                }
+
+                ergebnis = zahla - zahlb;
+
+                Console.WriteLine("Das Ergbenis ist: " + ergebnis);
+            }
+
+            else if (eingabe == "*")
+            {
+                Console.WriteLine("Geben Sie die nächste Zahl ein: ");
+                zahlb = Convert.ToInt32(Console.ReadLine());
+                i++;
+                while (i >= anzahl)
+                {
+                    i++;
+
+                    Console.WriteLine("Geben Sie die nächste Zahl ein: ");
+                    zahlb = Convert.ToInt32(Console.ReadLine());
+
+                    zahlb *= zahlb;
+
+                }
+
+                Console.WriteLine("Das Ergbenis ist: " + zahla * zahlb);
+            }
+
+            else if (eingabe == "/")
+            {
+                Console.WriteLine("Geben Sie die nächste Zahl ein: ");
+                zahlb = Convert.ToInt32(Console.ReadLine());
+                i++;
+                while (i >= anzahl)
+                {
+                    i++;
+
+                    Console.WriteLine("Geben Sie die nächste Zahl ein: ");
+                    zahlb = Convert.ToInt32(Console.ReadLine());
+
+                    zahlb /= zahlb;
+
+                }
+
+                Console.WriteLine("Das Ergbenis ist: " + zahla / zahlb);
+            }
+            else
+            {
+                System.Environment.Exit(0);
+            }
     }
 }
