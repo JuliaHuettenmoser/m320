09d3044cb626d499206dbe2003de88dbf2d42351
JuliaHuettenmoser <112378948+JuliaHuettenmoser@users.noreply.github.com>
2022-11-13 16:55:26 +0100

    aufgabe2.2

---
+++ M320/aufgabe02_02.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+
+namespace Aufgabe1
+{
+    internal class Rectangle
+    {
+        private int height;
+        readonly int width;
+        int lineWidth = 12;
+        static void Main(string[] args, int lineWidth)
+        {
+            
+        }
+
+        //ich habe keine Ahnung
+    }
+}
