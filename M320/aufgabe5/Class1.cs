0e9cbbaea735acaec4a19da4a7d54c6087037f10
JuliaHuettenmoser <112378948+JuliaHuettenmoser@users.noreply.github.com>
2022-11-13 17:39:50 +0100

    aufgabe 5

---
+++ M320/aufgabe5/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe5
+{
+    internal class Person
+    {
+        public static void Main (string[] args){
+            int eingabe;
+            int wutLevel = Aufgabe5.Emotionen.wutLevel;
+
+            Console.WriteLine("Was ist passiert: \n1) Lohnerhoehung\n2) Frau hat dich betrogen\n 3) Ferien\n4) Schlafen\n 5) Aufwachen(gewollt oder ungewollt)");
+            eingabe = Convert.ToInt32(Console.ReadLine());
+
+            Aufgabe5.Emotionen;
+
+            Console.WriteLine("Die neuen Level betragen:\nWut: " + wutLevel);
+
+        }
+
+    }
+}
---
+++ M320/aufgabe5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aufgabe5
+{
+    internal class Emotionen
+    {
+        public int wutLevel {get; set;}
+        public int entspanntLevel{get; set;}
+        public int froehlichLevel{get; set;}
+        
+
+        private int levelControl(int eingabe){
+            int eingabe = Aufgabe5.Person.eingabe;
+
+            switch(eingabe){
+                case 0:
+                
+            }
+        }
+    }
+}
